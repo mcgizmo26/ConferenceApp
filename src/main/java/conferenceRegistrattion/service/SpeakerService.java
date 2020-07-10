07a6759fdce7acd3cc23ae66a6d88b248354bf85
mcgizmo26 <lonnie.mcgill@smartshyp.com>
2020-07-10 17:09:42 -0600

    in beginning stage of app

---
+++ src/main/java/conferenceRegistrattion/service/SpeakerService.java
@@ -0,0 +1,11 @@
+package conferenceRegistrattion.service;
+
+import java.util.List;
+
+import conferenceRegistration.model.Speaker;
+
+public interface SpeakerService {
+
+	List<Speaker> findAll();
+
+}
---
+++ src/main/java/conferenceRegistrattion/service/SpeakerServiceImpl.java
@@ -0,0 +1,16 @@
+package conferenceRegistrattion.service;
+
+import java.util.List;
+
+import conferenceRegistration.model.Speaker;
+import conferenceRegistration.repository.HibernateSpeakerRepositoryImpl;
+import conferenceRegistration.repository.SpeakerRepository;
+
+public class SpeakerServiceImpl implements SpeakerService {
+
+	private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+
+	public List<Speaker> findAll() {
+		return repository.findAll();
+	}
+}
